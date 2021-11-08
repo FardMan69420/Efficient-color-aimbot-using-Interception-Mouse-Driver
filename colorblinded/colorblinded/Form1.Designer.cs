a51258ee19ae71de2232a141d387a7b82e22a3f2
cuciuu <63902322+cuciuu@users.noreply.github.com>
2021-11-08 05:33:21 +0200

    Add files via upload

---
+++ colorblinded/colorblinded/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿
+namespace colorblinded
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.start = new System.Windows.Forms.Button();
+            this.stop = new System.Windows.Forms.Button();
+            this.aim = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(12, 12);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(143, 20);
+            this.start.TabIndex = 0;
+            this.start.Text = "start";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // stop
+            // 
+            this.stop.Location = new System.Drawing.Point(161, 11);
+            this.stop.Name = "stop";
+            this.stop.Size = new System.Drawing.Size(143, 20);
+            this.stop.TabIndex = 1;
+            this.stop.Text = "stop";
+            this.stop.UseVisualStyleBackColor = true;
+            this.stop.Click += new System.EventHandler(this.stop_Click);
+            // 
+            // aim
+            // 
+            this.aim.Interval = 55;
+            this.aim.Tick += new System.EventHandler(this.aim_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(312, 44);
+            this.Controls.Add(this.stop);
+            this.Controls.Add(this.start);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "cuciu colorblind";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Button stop;
+        private System.Windows.Forms.Timer aim;
+    }
+}
+
---
+++ colorblinded/colorblinded/Form1.cs
@@ -0,0 +1,64 @@
+﻿using AutoItX3Lib;
+using Interceptor;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
+namespace colorblinded
+{
+    public partial class Form1 : Form
+    {
+        AutoItX3 au3 = new AutoItX3();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool isRunning = true;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+        Input input = new Input();
+
+        private void aim_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                int hex = 0xFFDBC3;
+                object pix = au3.PixelSearch(0, 0, 1919, 1079, hex);
+                if (pix.ToString() != "1")
+                {
+                    object[] pixCoord = (object[])pix;
+                    // If the pixel with that color is found then: 
+                    input.MoveMouseTo((int)pixCoord[0], (int)pixCoord[1]);  // Please note this doesn't use the driver to move the mouse; it uses System.Windows.Forms.Cursor.Position
+                    au3.MouseClick("LEFT");
+
+                }
+            }
+            catch { }
+            // If key {} is pressed, do:
+
+            Thread.Sleep(55);
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            aim.Start();
+        }
+
+        private void stop_Click(object sender, EventArgs e)
+        {
+            aim.Stop();
+        }
+    }
+}
